a4d2c14f4c34860146f564599b0817235431f4db
snorem <shaun.norem@gmail.com>
2015-06-26 21:08:50 -0500

    Added Prototype Files
    For easy use of previously set up prototype submission

---
+++ PrototypeFiles/Assets/Scripts/ShipData.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Contains all data pertaining to the player's ship
+public class ShipData : MonoBehaviour 
+{
+
+	// All variables hidden from inspector so that necessary changes
+	// are made in the GameManager and not here.
+
+	// The ship's base move speed
+	[HideInInspector]
+	public float moveSpeed;	
+
+	// The base move speed of the ship's primary weapon
+	[HideInInspector]
+	public float bulletSpeed;
+
+	// The amount of time needed to pass before the next
+	// bullet can be fired
+	[HideInInspector]
+	public float bulletCooldown;
+
+	// The damage of the primary weapon
+	[HideInInspector]
+	public int bulletDamage;
+
+	// The ship's current and max HP, respectively
+	[HideInInspector]	
+	public float HP, maxHP;	
+
+	// The player's current score
+	[HideInInspector]
+	public int score;
+
+	// How far from the front of the ship the bullets will spawn
+	[HideInInspector]
+	public Vector2 bulletOffset;
+
+	// Number of lives remaining for the player.
+	// When this is 0, it's game over man. 
+	[HideInInspector]
+	public int numLives;
+	
+	
+	// The All-Powerful GameManger will be used to initialize all of these values
+	void Start () 
+	{
+		moveSpeed = GameManager.instance.playerMoveSpeed;
+		bulletSpeed = GameManager.instance.playerBulletSpeed;
+		bulletCooldown = GameManager.instance.playerBulletCooldown;
+		bulletDamage = GameManager.instance.playerBulletDamage;
+		bulletOffset = GameManager.instance.playerBulletOffset;
+		numLives = GameManager.instance.numberOfLives;
+		maxHP = GameManager.instance.playerMaxHP;
+		HP = maxHP;
+	}		
+	
+}
---
+++ PrototypeFiles/Assets/Scripts/ShipEngine.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipEngine : MonoBehaviour 
+{
+	// Stores the ship's transform component so only one call to 
+	// GetComponent() is made
+	public Transform tf;
+	// ShipDate component
+	public ShipData data;
+	
+	
+	void Start () 
+	{
+		tf = GetComponent<Transform> ();
+		data = GetComponent<ShipData>();
+	}
+	
+	// Move the player up or down,
+	// depending on the positive/negative value of speed, respectively
+	public void moveVertical(float speed)
+	{
+		Vector2 newPosition = new Vector2(tf.position.x, tf.position.y + speed);
+		tf.position = newPosition;
+	}
+
+	// Move the player right or left,
+	// depending on the positive/negative value of speed, respectively
+	public void moveHorizontal(float speed)
+	{
+		Vector2 newPosition = new Vector2(tf.position.x + speed, tf.position.y);
+		tf.position = newPosition;
+	}
+	
+	// Damages the player's ship by amount parameter
+	public void Damage(int amount)
+	{
+		// Adjust HP by amount and check to see if we're less than 0,
+		// meaning the ship is destroyed and the player must respawn.
+		if((data.HP -= amount) <= 0)
+		{
+			Respawn();
+		}
+	}
+	
+	
+	// To be implemented later
+	public void Respawn()
+	{
+		return; 
+	}
+	
+	// Also to be implemented later
+	public void GameOver()
+	{
+		return;
+	}	                                 
+	
+	
+}
