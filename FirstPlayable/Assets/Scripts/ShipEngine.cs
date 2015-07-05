2ce0aab6db8e71af17094c63e201b8664fa013bd
estes278 <estes278@gmail.com>
2015-07-05 15:08:28 -0400

    Collision Detection
    Incomplete but it's a good start

---
+++ FirstPlayable/Assets/Scripts/ShipEngine.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipEngine : MonoBehaviour 
+{
+	// Stores the ship's transform component so only one call to 
+	// GetComponent() is made
+	public Transform tf;
+	// ShipDate component
+	public ShipData data;
+	public bool isInvincible;
+	float invincibleCooldown;
+	
+	
+	void Start () 
+	{
+		tf = GetComponent<Transform> ();
+		data = GetComponent<ShipData>();
+		isInvincible = false;
+		invincibleCooldown = 0.0f;
+	}
+
+	void Update()
+	{
+		// If we're in invincible mode, call invincibleMode() to handle
+		// the cooldown and changing of sprite color
+		if(isInvincible)
+			invincibleMode();
+	}
+	
+	// Move the player up or down,
+	// depending on the positive/negative value of speed, respectively
+	public void moveVertical(float speed)
+	{
+		Vector2 newPosition = new Vector2(tf.position.x, tf.position.y + (speed * Time.deltaTime));
+		tf.position = newPosition;
+	}
+
+	// Move the player right or left,
+	// depending on the positive/negative value of speed, respectively
+	public void moveHorizontal(float speed)
+	{
+		Vector2 newPosition = new Vector2(tf.position.x + (speed * Time.deltaTime), tf.position.y);
+		tf.position = newPosition;
+	}
+	
+	// Damages the player's ship by amount parameter
+	public void Damage(int amount)
+	{
+		// Adjust HP by amount and check to see if we're less than 0,
+		// meaning the ship is destroyed and the player must respawn.
+		if((data.HP -= amount) <= 0)
+		{
+			Respawn();
+		}
+	}
+
+	// Handles obstacle and enemy collisions
+	// After colliding, the player takes 5 damage (or 1/3 of total health)
+	// and is invlunerable for invlunTimeLength seconds, a value
+	// included in and adjustable from the GameManager
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		// If we're in invincible mode, ignore collisions
+		if(!isInvincible)
+		{
+			// If we hit the floor, bounce up a bit 
+			// and activate invincibility
+			if(col.gameObject.tag == "FloorCol")
+			{
+				Vector2 bounceUp = new Vector2(tf.position.x,
+				                               tf.position.y + GameManager.instance.bounceAmount);
+				tf.position = bounceUp;
+
+			}
+			// Same thing with the ceiling, except bounce down
+			if(col.gameObject.tag == "CeilingCol")
+			{
+				Vector2 bounceDown = new Vector2(tf.position.x,
+				                               tf.position.y - GameManager.instance.bounceAmount);
+				tf.position = bounceDown;
+				invincibleMode();
+			}
+		}
+	}
+
+
+	void invincibleMode()
+	{
+		// This line indicates that invincibleMode has just been called
+		// from a collision detection. Changes the sprite color to red
+		// and sets invincible mode to true
+		if(!isInvincible)
+		{
+			GetComponent<SpriteRenderer>().material.color = Color.red;
+			isInvincible = true;
+		}
+		// if we're in invincible mode, add to the cooldown. If we're finished
+		// cooling down, change the sprite color back to white and reset the
+		// cooldown and boolean value
+		if(isInvincible)
+		{
+			invincibleCooldown += Time.deltaTime;
+			if(invincibleCooldown >= GameManager.instance.invincibleTimeLength)
+			{
+				isInvincible = false;
+				invincibleCooldown = 0.0f;
+				GetComponent<SpriteRenderer>().material.color = Color.white;
+			}
+		}	
+	}
+	// To be implemented later
+	public void Respawn()
+	{
+		return; 
+	}
+	
+	// Also to be implemented later
+	public void GameOver()
+	{
+		return;
+	}	                                 
+	
+	
+}
---
+++ FirstPlayable/Assets/Scripts/ShipShooter.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipShooter : MonoBehaviour 
+{
+	// The prefab for the ship's primary bullet weapon
+	public GameObject bulletPrefab;
+
+	// The current bullet being created
+	private GameObject bullet;
+
+	// The ShipData component
+	public ShipData data;
+
+	// The Ship's transform component
+	public Transform tf;
+
+	// The current cooldown that the bullet is on
+	private float currentCooldown;
+
+	
+	void Start () 
+	{
+		data = GetComponent<ShipData>();
+		tf = GetComponent<Transform>();
+		// Start out ready to fire!
+		currentCooldown = data.bulletCooldown;
+	}
+	
+	void Update () 
+	{
+		// If we're in cooldown mode, add Time.deltaTime
+		// until we've reached the cooldown timer
+		if(currentCooldown < data.bulletCooldown)
+			currentCooldown += Time.deltaTime;
+	}
+
+	// Fires the primary ship weapon, a small bullet. Fires in intervals
+	// of data.bulletCooldown, which will likely be a small number less than
+	// one second
+	public void Shoot()
+	{
+		// Do nothing if we're in cooldown mode
+		if(currentCooldown < data.bulletCooldown) return;
+		else
+		{
+			// Instantiate a bullet at the given offset position and rotation and then
+			// add force to it to make it move
+			bullet = (GameObject)Instantiate(bulletPrefab, 
+			                                 (Vector2)tf.position + (Vector2)tf.forward + data.bulletOffset, 
+			                                 Quaternion.identity);
+			// Set this here so that Bullet doesn't have to look for it later
+			bullet.GetComponent<BulletController>().parentData = data;
+			currentCooldown = 0;
+		}
+	}
+}
