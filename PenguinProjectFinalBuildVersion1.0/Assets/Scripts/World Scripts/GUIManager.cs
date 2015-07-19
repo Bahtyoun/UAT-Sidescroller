9d0dfc0d0fc89a294a9db2a9ad7081c72e86da3c
estes278 <estes278@gmail.com>
2015-07-19 15:09:24 -0400

    Death works properly

---
+++ PenguinProjectFinalBuildVersion1.0/Assets/Scripts/World Scripts/GUIManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIManager : MonoBehaviour 
+{
+	public Slider healthBar;
+	public Image Life1, Life2, Life3;
+	public Text playerScore;
+
+}
---
+++ PenguinProjectFinalBuildVersion1.0/Assets/Scripts/World Scripts/HealthBubbleBehavior.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBubbleBehavior : MonoBehaviour {
+
+	[HideInInspector]
+	float bubbleSpeed;
+
+	[HideInInspector]
+	Vector2 bubbleDirection;
+
+	// Use this for initialization
+	void Start () {
+		bubbleSpeed = 2.0f;
+		bubbleDirection.x = 0.0f;
+		bubbleDirection.y = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Quickly thrown together, needs to be improved upon!
+		this.transform.Translate (bubbleDirection * bubbleSpeed * Time.deltaTime);
+		if (this.transform.position.y > 10.0f)
+			Destroy (gameObject);
+	}
+}
